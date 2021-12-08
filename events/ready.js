1d471a2ba6dad071ac73f6855d23d239499c1a15
dry14 <92650927+dry14@users.noreply.github.com>
2021-12-09 06:29:22 +0700

    Initial commit

---
+++ events/ready.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: 'ready',
+  execute(client) {
+    console.log('Ticket Bot ready!')
+    const oniChan = client.channels.cache.get(client.config.ticketChannel)
+
+    function sendTicketMSG() {
+      const embed = new client.discord.MessageEmbed()
+        .setColor('6d6ee8')
+        .setAuthor('Ticket', client.user.avatarURL())
+        .setDescription('Click the button below to open a ticket')
+        .setFooter(client.config.footerText, client.user.avatarURL())
+      const row = new client.discord.MessageActionRow()
+        .addComponents(
+          new client.discord.MessageButton()
+          .setCustomId('open-ticket')
+          .setLabel('Open Ticket')
+          .setEmoji('✉️')
+          .setStyle('PRIMARY'),
+        );
+
+      oniChan.send({
+        embeds: [embed],
+        components: [row]
+      })
+    }
+
+    oniChan.bulkDelete(100).then(() => {
+      sendTicketMSG()
+    })
+  },
+};
